2477b39befba0b2e32c0d4ffc61196cde43fd832
David Ha <dha@college.harvard.edu>
2025-04-28 23:37:21 -0400

    Create moves.py

---
+++ moves.py
@@ -0,0 +1,16 @@
+player_moves = [
+    "Jab", "Cross", "Hook", "Uppercut", "Body Shot",
+    "Slip", "Parry", "Block", "Counter Punch"
+]
+
+move_effects = {
+    "Jab": {"stamina_cost": 2, "damage": 5},
+    "Cross": {"stamina_cost": 3, "damage": 8},
+    "Hook": {"stamina_cost": 4, "damage": 10},
+    "Uppercut": {"stamina_cost": 4, "damage": 11},
+    "Body Shot": {"stamina_cost": 3, "damage": 7},
+    "Slip": {"stamina_cost": 2, "damage": 0},
+    "Parry": {"stamina_cost": 2, "damage": 0},
+    "Block": {"stamina_cost": 1, "damage": 0},
+    "Counter Punch": {"stamina_cost": 4, "damage": 12},
+}
